5d8047d3da3d63daa591d5f1b0df2c0625401871
chromerai <kaustubhmishra983@gmail.com>
2024-06-12 21:44:11 -0400

    updated code for admin login
    updated1

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import prisma from "./prisma"
-import apiClient from "./api-client"
-import {connection} from "./redis"
-import { jobsQueue } from "./queue"
-
-export { prisma, apiClient, connection, jobsQueue };
