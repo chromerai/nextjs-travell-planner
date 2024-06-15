7b4165b40058baa0f7bf763321d8d9370f558b1f
chromerai <kaustubhmishra983@gmail.com>
2024-06-15 00:34:41 -0400

    updated code to include scaping-queue

---
+++ src/app/api/admin/job-details/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib"
+
+export async function GET() {
+    try {
+        const jobs = await prisma.jobs.findMany({ orderBy: { createdAt: 'desc' }});
+        const onGoingJobs = await prisma.jobs.findMany({ where: { isComplete: false }});
+        return NextResponse.json(
+            {jobs, onGoingJobs: onGoingJobs?.length ?? 0,
+            },
+            {status: 200}
+        );
+
+    } catch (error) {
+        return NextResponse.json(
+            {error: "An unexpected error occurred."},
+            {status: 500 }
+        );
+    }
+}
--- src/utils/api-routes.ts
+++ src/utils/api-routes.ts
@@ -2,4 +2,6 @@
 
 export const ADMIN_API_ROUTES  = {
     LOGIN: "/admin/login",
+    CREATE_JOB: "/admin/create-job",
+    JOB_DETAILS: "/admin/job-details",
 };
